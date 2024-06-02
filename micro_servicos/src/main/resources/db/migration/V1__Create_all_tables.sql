659313fe9a50a5be1c752840225d988e4c832ec2
Djonathan D'Avila <123762143+DjonathanDAvila@users.noreply.github.com>
2024-06-02 15:19:02 -0300

    :recycle: refactor:(Va__Creat_All_tables): Refatorado script de criação das tabelas

--- micro_servicos/src/main/resources/db/migration/V1__Create_all_tables.sql
+++ micro_servicos/src/main/resources/db/migration/V1__Create_all_tables.sql
@@ -24,39 +24,27 @@ CREATE TABLE arqs_alan_new.t_media
 
 CREATE TABLE arqs_alan_new.t_media_type
 (
-    id VARCHAR(64) PRIMARY KEY NOT NULL
+    id binary(16) NOT NULL  DEFAULT (UUID_TO_BIN(UUID()))
     , name VARCHAR(30) NOT NULL UNIQUE
     , created_date DATETIME DEFAULT NOW()
     , inactivated_date DATETIME NULL
+    , CONSTRAINT PK_media_type PRIMARY KEY (id)
 );
 
 CREATE TABLE arqs_alan_new.t_rating
 (
-    id VARCHAR(64) PRIMARY KEY NOT NULL
+    id binary(16) NOT NULL  DEFAULT (UUID_TO_BIN(UUID()))
     , name VARCHAR(20) NOT NULL UNIQUE
     , created_date DATETIME DEFAULT NOW()
     , inactivated_date DATETIME NULL
+    , CONSTRAINT PK_rating PRIMARY KEY (id)
 );
 
 CREATE TABLE arqs_alan_new.t_participant
 (
-    id VARCHAR(64) PRIMARY KEY NOT NULL
+    id binary(16) NOT NULL  DEFAULT (UUID_TO_BIN(UUID()))
     , name VARCHAR(50) NOT NULL UNIQUE
     , created_date DATETIME DEFAULT NOW()
-    , inactivated_date DATETIME NULL
-);
-
-DROP DATABASE arqs_alan;
-CREATE DATABASE arqs_alan;
-USE arqs_alan;
-
-CREATE TABLE arqs_alan.t_media
-(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT
-    , genre VARCHAR(30) NOT NULL
-    , category VARCHAR(30) NOT NULL
-    , media_name VARCHAR(250) NOT NULL
-    , type VARCHAR(30) NOT NULL
-    , rating VARCHAR(20) NOT NULL
-    , participant VARCHAR(50) NOT NULL
+    , inactivated_date DATETIME NULL,
+    CONSTRAINT PK_participant PRIMARY KEY (id)
 );
